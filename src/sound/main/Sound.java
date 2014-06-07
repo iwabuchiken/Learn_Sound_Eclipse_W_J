4d63b94d19f45032e21062ccd0b16a1d41b8bc38
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-06-08 00:19:56 +0900

    D-7
    v-2.0
    ---
    1. JDBC
    
    	==> done

--- src/sound/main/Sound.java
+++ src/sound/main/Sound.java
@@ -5,8 +5,15 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.sql.*;
 
 import com.panayotis.gnuplot.JavaPlot;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Sound {
 
@@ -14,6 +21,8 @@ public class Sound {
 		// TODO Auto-generated method stub
 
 		System.out.println("JavaPlot");
+		
+		_D_7_JDBC();
 		
 //		JavaPlot p = new JavaPlot();
 //        p.addPlot("sin(x)");
@@ -70,6 +79,75 @@ public class Sound {
 //        }
 		
 	}//public static void main(String[] args)
+
+	private static void _D_7_JDBC() {
+		// TODO Auto-generated method stub
+		try {
+			
+			String className = "org.sqlite.JDBC";
+			
+			Class.forName(className);
+//			Class.forName("org.sqlite.JDBC");
+			
+			String message = "Class found => " + className;
+			message(message,
+					Thread.currentThread().getStackTrace()[1].getLineNumber());
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			System.exit(-1);
+			
+		}
+		
+		Connection connection = null;
+		
+		try {
+			
+			String dbName = "sample1.db";
+			
+			connection = DriverManager.getConnection("jdbc:sqlite:" + dbName);
+//			connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+			Statement statement = connection.createStatement();
+			statement.setQueryTimeout(30);  // set timeout to 30 sec.
+			
+			statement.executeUpdate("drop table if exists person");
+			statement.executeUpdate("create table person (id integer, name string)");
+			statement.executeUpdate("insert into person values(1, 'leo')");
+			statement.executeUpdate("insert into person values(2, 'yui')");
+			
+			String message = "Executions => done";
+			message(message,
+					Thread.currentThread().getStackTrace()[1].getLineNumber());
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+			
+		} finally {
+			
+			try
+			{
+				if(connection != null)
+					connection.close();
+				
+				String message = "Connection => closed";
+				message(message,
+						Thread.currentThread().getStackTrace()[1]
+								.getLineNumber());
+				
+			}
+			catch(SQLException e)
+			{
+				// 	connection close failed.
+				System.err.println(e);
+			}
+		}
+		
+	}//private static void _D_7_JDBC()
 
 	public static void message(String message, int line) {
 		
