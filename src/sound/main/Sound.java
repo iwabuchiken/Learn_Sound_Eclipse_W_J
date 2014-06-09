6f140d73c91152db09124799d053f549fe459e48
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-06-09 11:31:16 +0900

    D-7
    v-4.0
    ---
    1. New Class => WavFile.java
    
    	==> done

--- src/sound/main/Sound.java
+++ src/sound/main/Sound.java
@@ -14,6 +14,13 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.io.*;
+
+import org.apache.commons.lang.StringUtils;
+
+import sound.utils.Methods;
+import stream.io.WavFile;
+import stream.io.WavFileException;
 
 public class Sound {
 
@@ -29,24 +36,137 @@ public class Sound {
 
 		System.out.println("JavaPlot");
 		
-		_D_7_JNI();
+//		_D_7_v_4_Check_FilePath();
+		_D_7_v_4_WavFile();
 		
+//		WavFile wFile;
+//		wavFiles.WavFile wFile;
+		
+//		_D_7_JNI();
+		
+//		_D_7_Initial_Codes();
 //		_D_7_JDBC();
 		
+		String message = "done";
+		message(message,
+				Thread.currentThread().getStackTrace()[1].getLineNumber());
+		
+
+		
+	}//public static void main(String[] args)
+
+	private static void _D_7_v_4_WavFile() {
+		// TODO Auto-generated method stub
+		String fname_Wav = "a.wav";
+		
+		String fpath_Wav = StringUtils.join(
+				new String[]{
+					"audio",
+					fname_Wav
+				}, File.separator);
+		
+		File audioFile_Src = new File(fpath_Wav);
+		
+		WavFile wavFile = null;
+		
+		try {
+			
+			wavFile = WavFile.openWavFile(audioFile_Src);
+			
+			String message = "File => opened: " + audioFile_Src.getAbsolutePath();
+			message(message,
+					Thread.currentThread().getStackTrace()[1].getLineNumber());
+			
+			
+			wavFile.close();
+			
+			message = "File => closed: " + audioFile_Src.getAbsolutePath();
+			message(message,
+					Thread.currentThread().getStackTrace()[1].getLineNumber());
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (WavFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	private static void _D_7_v_4_Check_FilePath() {
+		// TODO Auto-generated method stub
+		String fname_Wav = StringUtils.join(
+				new String[]{
+					
+					"abc_",
+					Methods.get_TimeLabel(Methods.getMillSeconds_now()),
+					".txt"
+						
+				});
+		
+//		String fpath_Wav = "";
+		
+		File f = new File(fname_Wav);
+		
+		FileWriter fw = null;
+		
+		try {
+			fw = new FileWriter(f);
+			
+			String message = "Time is "
+						+ Methods.get_TimeLabel(Methods.getMillSeconds_now());
+			
+//					"abc_" + Methods.get_TimeLabel(Methods.getMillSeconds_now()) + ".txt";
+			
+			fw.write(message);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		} finally {
+			
+			try {
+				fw.close();
+				
+				String message = "File closed";
+				
+				message(message,
+						Thread.currentThread().getStackTrace()[1].getLineNumber());
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+//		try {
+//			WavFile wavFile = WavFile.openWavFile(new File(args[0]));
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (WavFileException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+
+	}
+
+	private static void _D_7_Initial_Codes() {
+		// TODO Auto-generated method stub
 //		JavaPlot p = new JavaPlot();
-//        p.addPlot("sin(x)");
-//        p.plot();
+//      p.addPlot("sin(x)");
+//      p.plot();
 //		JavaPlot p = new JavaPlot(true);
 //		
 //		p.setTitle(Thread.currentThread().getStackTrace()[1].getFileName());
 //		
-//        p.addPlot("sin(x)*y");
-//        p.plot();
-        
-        String message = "done";
-		message(message,
-				Thread.currentThread().getStackTrace()[1].getLineNumber());
-		
+//      p.addPlot("sin(x)*y");
+//      p.plot();
+      
 
 //		String message = "message";
 //		
@@ -62,12 +182,12 @@ public class Sound {
 //		System.out.println(label + " " + message);
 		
 		
-//        // Log
+//      // Log
 //		String msg_Log = "message";
 //		Log.d("Sound.java" + "["
 //				+ Thread.currentThread().getStackTrace()[2].getLineNumber()
 //				+ "]", msg_Log);
-        
+      
 //		Map<String, String> env = System.getenv();
 //		
 //		Set<String> kSet = env.keySet();
@@ -82,12 +202,11 @@ public class Sound {
 		//REF http://docs.oracle.com/javase/tutorial/essential/environment/env.html
 //		for (String envName : list) {
 ////			for (String envName : env.keySet()) {
-//            System.out.format("%s=%s%n",
-//                              envName,
-//                              env.get(envName));
-//        }
-		
-	}//public static void main(String[] args)
+//          System.out.format("%s=%s%n",
+//                            envName,
+//                            env.get(envName));
+//      }
+	}
 
 	private static void _D_7_JNI() {
 		// TODO Auto-generated method stub
--- src/sound/utils/Methods.java
+++ src/sound/utils/Methods.java
@@ -1,5 +1,37 @@
 package sound.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Methods {
+
+	/****************************************
+	 *	getMillSeconds_now()
+	 * 
+	 * <Caller> 
+	 * 1. ButtonOnClickListener # case main_bt_start
+	 * 
+	 * <Desc> 1. <Params> 1.
+	 * 
+	 * <Return> 1.
+	 * 
+	 * <Steps> 1.
+	 ****************************************/
+	public static long getMillSeconds_now() {
+		
+		Calendar cal = Calendar.getInstance();
+		
+		return cal.getTime().getTime();
+		
+	}//private long getMillSeconds_now(int year, int month, int date)
+
+	public static String get_TimeLabel(long millSec) {
+		
+		 SimpleDateFormat sdf1 = new SimpleDateFormat("yyyyMMdd_HHmmss");
+		 
+		return sdf1.format(new Date(millSec));
+		
+	}//public static String get_TimeLabel(long millSec)
 
 }
