1642b027b7572c5aa47b978612c7610d23ea3fd8
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-06-09 15:18:16 +0900

    D-7
    v-4.3.0
    ---
    1. plot => try 1
    
    	==> done

--- src/sound/main/Sound.java
+++ src/sound/main/Sound.java
@@ -35,8 +35,9 @@ public class Sound {
 		// TODO Auto-generated method stub
 
 		System.out.println("JavaPlot");
-		
-		_D_7_v_4_EachChannelData();
+
+		new Sound()._D_7_v_4_Plot_Data();
+//		_D_7_v_4_EachChannelData();
 //		_D_7_v_4_WavFile();
 //		_D_7_v_4_Check_FilePath();
 		
@@ -164,6 +165,226 @@ public class Sound {
 						Thread.currentThread().getStackTrace()[1]
 								.getLineNumber());
 			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (WavFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		
+	}//_D_7_v_4_EachChannelData()
+	
+	private void
+	_D_7_v_4_Plot_Data() {
+		// TODO Auto-generated method stub
+		String fname_Wav = "2.wav";
+//		String fname_Wav = "a.wav";
+		
+		String fpath_Wav = StringUtils.join(
+				new String[]{
+						"audio",
+						fname_Wav
+				}, File.separator);
+		
+		File audioFile_Src = new File(fpath_Wav);
+		
+		WavFile wavFile = null;
+		
+		try {
+			
+			wavFile = WavFile.openWavFile(audioFile_Src);
+			
+			String message = "File => opened: " + audioFile_Src.getAbsolutePath();
+			message(message,
+					Thread.currentThread().getStackTrace()[1].getLineNumber());
+			
+			// display
+			wavFile.display();
+			
+			////////////////////////////////
+			
+			// Setup: vars
+			
+			////////////////////////////////
+			int start = 10000;
+			int num_of_display = 10;
+			
+			int framesRead;
+			int numChannels = wavFile.getNumChannels();
+			long num_Frames = wavFile.getNumFrames();
+			
+			double[] buf = new double[(int) num_Frames * numChannels];
+			double[] buf_L = new double[(int) num_Frames];
+			double[] buf_R = new double[(int) num_Frames];
+			
+			////////////////////////////////
+			
+			// Read: frames
+			
+			////////////////////////////////
+			framesRead = wavFile.readFrames(buf, (int) wavFile.getNumFrames());
+			
+			message = "framesRead = " + framesRead;
+			message(message,
+					Thread.currentThread().getStackTrace()[1].getLineNumber());
+			
+			////////////////////////////////
+			
+			// Close: wav
+			
+			////////////////////////////////
+			wavFile.close();
+			
+			message = "File => closed: " + audioFile_Src.getAbsolutePath();
+			message(message,
+					Thread.currentThread().getStackTrace()[1].getLineNumber());
+			
+			////////////////////////////////
+			
+			// Process: data
+			
+			////////////////////////////////
+//			for (int i = 0; i < num_Frames; i += 2) {
+//				for (int i = 0; i < num_Frames; i++) {
+			for (int i = 0, j = 0; i < num_Frames; i += 2, j ++) {
+				
+				buf_L[j] = buf[i];
+				buf_R[j] = buf[i + 1];
+				
+				
+//				//REF format http://stackoverflow.com/questions/47045/sprintf-equivalent-in-java answered Sep 5 '08 at 23:06
+//				message = String.format(
+//							"buf[%d] = %f / buf_L[%d] = %f / buf_R[%d] = %f",
+//							i, buf[i], i, buf_L[i], i, buf_R[i]);
+//				message(message,
+//						Thread.currentThread().getStackTrace()[1]
+//								.getLineNumber());
+			}
+			
+			message = "Processing => done";
+			message(message,
+					Thread.currentThread().getStackTrace()[1].getLineNumber());
+			
+			
+			////////////////////////////////
+			
+			// Display: data
+			
+			////////////////////////////////
+//			int start = 0;
+//			int num_of_display = 10;
+			
+			for (int i = start; i < start + num_of_display; i++) {
+				
+				//REF format http://stackoverflow.com/questions/47045/sprintf-equivalent-in-java answered Sep 5 '08 at 23:06
+				message = String.format(
+						"buf[%d] = %f / buf_L[%d] = %f / buf_R[%d] = %f",
+						i, buf[i], i, buf_L[i], i, buf_R[i]);
+				message(message,
+						Thread.currentThread().getStackTrace()[1]
+								.getLineNumber());
+			}
+			
+			////////////////////////////////
+
+			// Prep: plot data
+
+			////////////////////////////////
+			//REF multi http://stackoverflow.com/questions/12231453/creating-two-dimensional-array answered Sep 1 '12 at 21:15
+			
+			int count = 0;
+			start = 10000; num_of_display = 5;
+			
+			double[][] data = new double[(int) num_of_display][2];
+			
+			for (int i = start; i < start + num_of_display; i++) {
+//				for (int i = 361000; i < 36110; i++) {
+//				for (int i = 361000; i < buf_R.length; i++) {
+				
+				data[count][0] = count;
+				data[count][1] = buf_L[i];
+				
+				count ++;
+				
+			}
+			
+			start = 0; num_of_display = 5;
+			
+			for (int i = start; i < start + num_of_display; i++) {
+				
+				message = String.format(
+						"data[%d][0] = %f / data[%d][1] = %f", 
+						i, data[i][0], i, data[i][1]);
+				
+				message(message,
+						Thread.currentThread().getStackTrace()[1]
+								.getLineNumber());
+				
+				
+			}
+			
+//			 0.000000 / data[0][1] = 0.107300
+//					 [Sound.java : message : 318] data[1][0] = 1.000000 / data[1][1] = 0.058380
+//					 [Sound.java : message : 318] data[2][0] = 2.000000 / data[2][1] = 0.103516
+//					 [Sound.java : message : 318] data[3][0] = 3.000000 / data[3][1] = 0.055573
+//					 [Sound.java : message : 318] data[4][0] = 4.000000 / data[4][1] = 0.100372
+//					 [Sound.java : message : 318] data[5][0] = 5.000000 / data[5][1] = 0.053314
+//					 [Sound.java : message : 318] data[6][0] = 6.000000 / data[6][1] = 0.097321
+//					 [Sound.java : message : 318] data[7][0] = 7.000000 / data[7][1] = 0.051605
+//					 [Sound.java : message : 318] data[8][0] = 8.000000 / data[8][1] = 0.093140
+//					 [Sound.java : message : 318] data[9][0] = 9.000000 / data[9][1] = 0.050568			
+//			double[][] data = new double[][]{
+//					
+//					{1.0, 0.058},
+//					{2.0, 0.103},
+//					
+//			};
+//			
+//			start = 0; num_of_display = 2;
+//			
+//			for (int i = start; i < start + num_of_display; i++) {
+//			
+//				message = String.format(
+//						"data[%d][0] = %f / data[%d][1] = %f", 
+//						i, data[i][0], i, data[i][1]);
+//				
+//				message(message,
+//						Thread.currentThread().getStackTrace()[1]
+//								.getLineNumber());
+//				
+//				
+//			}
+
+			
+//			double[][] data = new double[][]{
+//					
+//					{0.3, 0.4},
+//					{0.2, 0.1},
+//					
+//			};
+			
+			
+			////////////////////////////////
+
+			// Plot
+
+			////////////////////////////////
+			
+			
+			
+			
+			JavaPlot p = new JavaPlot();
+//			p.addPlot("sin(x)");
+//			p.plot();
+//				JavaPlot p = new JavaPlot(true);
+				
+			p.setTitle(Thread.currentThread().getStackTrace()[1].getFileName());
+			
+			p.addPlot(data);
+//			p.addPlot("sin(x)*y");
+			p.plot();
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
